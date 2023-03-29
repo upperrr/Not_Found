8145fa45141ab2f8ca402bb293db6cb699395dc8
th <upperrr@gmail.com>
2023-03-30 02:11:15 +0900

    Initial commit

---
+++ src/test/java/com/found/not_found/NotFoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.found.not_found;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotFoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
